88f04593eae916012a9c453919eb2a56bbed8719
cooolbros <pfwobcke@gmail.com>
2021-12-16 16:20:03 +1100

    Update resources URLs

--- src/hud-merger/Properties/Resources.Designer.cs
+++ src/hud-merger/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace HUDMerger.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -61,7 +61,7 @@ namespace HUDMerger.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to https://raw.githubusercontent.com/cooolbros/hud-merger/main/hud-merger/Resources/Clientscheme.json.
+        ///   Looks up a localized string similar to https://raw.githubusercontent.com/cooolbros/hud-merger/main/src/hud-merger/Resources/Clientscheme.json.
         /// </summary>
         internal static string ClientschemeURL {
             get {
@@ -70,7 +70,7 @@ namespace HUDMerger.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to https://raw.githubusercontent.com/cooolbros/hud-merger/main/hud-merger/Resources/Panels.json.
+        ///   Looks up a localized string similar to https://raw.githubusercontent.com/cooolbros/hud-merger/main/srchud-merger/Resources/Panels.json.
         /// </summary>
         internal static string PanelsURL {
             get {
