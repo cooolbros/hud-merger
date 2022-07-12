c2d3fc6e647ba2d3601f11cc44bb21e937ef6e12
cooolbros <pfwobcke@gmail.com>
2022-07-12 16:03:52 +1000

    Update FilesHashSet

--- src/hud-merger/Models/FilesHashSet.cs
+++ src/hud-merger/Models/FilesHashSet.cs
@@ -1,36 +1,37 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Text.RegularExpressions;
 
 namespace HUDMerger.Models
 {
 	public class FilesHashSet : HashSet<string>
 	{
+		private class FilePathComparer : IEqualityComparer<string>
+		{
+			public bool Equals(string x, string y)
+			{
+				return x == Encode(y);
+			}
+
+			public int GetHashCode([DisallowNull] string obj)
+			{
+				return Encode(obj).GetHashCode();
+			}
+
+			public static string Encode(string filePath)
+			{
+				return String.Join('\\', Regex.Split(filePath, @"[/\\]+"));
+			}
+		}
+
 		public FilesHashSet() : base(new FilePathComparer())
 		{
 		}
 
 		public new bool Add(string item)
 		{
-			return base.Add(FilesHashSet.EncodeFilePath(item));
-		}
-
-		public static string EncodeFilePath(string filePath)
-		{
-			return String.Join('\\', Regex.Split(filePath, @"[/\\]+"));
-		}
-	}
-
-	class FilePathComparer : IEqualityComparer<string>
-	{
-		public bool Equals(string x, string y)
-		{
-			return x == FilesHashSet.EncodeFilePath(y);
-		}
-
-		public int GetHashCode(string x)
-		{
-			return FilesHashSet.EncodeFilePath(x).GetHashCode();
+			return base.Add(FilePathComparer.Encode(item));
 		}
 	}
 }
