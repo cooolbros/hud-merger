dd1dada88b13d6023783df5ad2f08a684ce5ea19
cooolbros <pfwobcke@gmail.com>
2023-12-19 10:52:17 +1100

    Make Dependencies optional

--- src/HUDMerger/Models/HUDPanel.cs
+++ src/HUDMerger/Models/HUDPanel.cs
@@ -8,6 +8,6 @@ public record class HUDPanel
 	public required string Name { get; init; }
 	public required string Main { get; init; }
 	public KeyValueLocation? RequiredKeyValue { get; init; }
-	public required Dependencies Dependencies { get; init; }
+	public Dependencies? Dependencies { get; init; }
 	public HashSet<string>? Files { get; init; }
 }
