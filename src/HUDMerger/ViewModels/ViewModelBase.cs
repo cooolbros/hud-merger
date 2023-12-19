ae8b657a528d943e49e752a96e156b1e866d4f00
cooolbros <pfwobcke@gmail.com>
2023-12-19 12:19:11 +1100

    Update ViewModelBase
     - Update OnPropertyChanged to use CallerMemberName
     - Implement dispose pattern (https://learn.microsoft.com/en-us/dotnet/standard/garbage-collection/implementing-dispose)

--- src/HUDMerger/ViewModels/ViewModelBase.cs
+++ src/HUDMerger/ViewModels/ViewModelBase.cs
@@ -1,18 +1,36 @@
 using System;
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace HUDMerger.ViewModels;
 
 public abstract class ViewModelBase : INotifyPropertyChanged, IDisposable
 {
-	public event PropertyChangedEventHandler PropertyChanged;
+	private bool _disposed;
+	public event PropertyChangedEventHandler? PropertyChanged;
 
-	protected void OnPropertyChanged(string propertyName)
+	protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
 	{
 		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 	}
 
-	public virtual void Dispose()
+	public void Dispose()
 	{
+		Dispose(disposing: true);
+		GC.SuppressFinalize(this);
+	}
+
+	protected virtual void Dispose(bool disposing)
+	{
+		if (_disposed)
+		{
+			return;
+		}
+
+		if (disposing)
+		{
+		}
+
+		_disposed = true;
 	}
 }
