bb7f95edd5d1e2667c5b56afdee0d8f6d41fe2c5
cooolbros <pfwobcke@gmail.com>
2023-12-20 00:08:15 +1100

    Remove unused usings

--- src/HUDMerger/ViewModels/TargetHUDPanelsListViewModel.cs
+++ src/HUDMerger/ViewModels/TargetHUDPanelsListViewModel.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Windows.Data;
-using HUDMerger.Models;
 
 namespace HUDMerger.ViewModels;
 
