c8b49df18d59fccdac7b9b6a1a3c8b793d7682a2
cooolbros <pfwobcke@gmail.com>
2023-12-20 14:49:00 +1100

    Add Scheme constructors

--- src/HUDMerger/Models/Scheme/ClientScheme.cs
+++ src/HUDMerger/Models/Scheme/ClientScheme.cs
@@ -5,4 +5,12 @@ namespace HUDMerger.Models.Scheme;
 public class ClientScheme : SchemeBase
 {
 	public override string Type => "client";
+
+	public ClientScheme()
+	{
+	}
+
+	public ClientScheme(string folderPath) : base(folderPath)
+	{
+	}
 }
--- src/HUDMerger/Models/Scheme/SourceScheme.cs
+++ src/HUDMerger/Models/Scheme/SourceScheme.cs
@@ -5,4 +5,12 @@ namespace HUDMerger.Models.Scheme;
 public class SourceScheme : SchemeBase
 {
 	public override string Type => "source";
+
+	public SourceScheme()
+	{
+	}
+
+	public SourceScheme(string folderPath) : base(folderPath)
+	{
+	}
 }
