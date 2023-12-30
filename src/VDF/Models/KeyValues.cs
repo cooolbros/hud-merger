7376d74b0101830f5d7f8a034324aa29b7fff569
cooolbros <pfwobcke@gmail.com>
2023-12-30 13:35:42 +1100

    Override KeyValues Equals

--- src/VDF/Models/KeyValues.cs
+++ src/VDF/Models/KeyValues.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 namespace VDF.Models;
 
@@ -8,4 +9,35 @@ public class KeyValues : List<KeyValue>
 {
 	public KeyValues() { }
 	public KeyValues(IEnumerable<KeyValue> collection) : base(collection) { }
+
+	public override bool Equals([NotNullWhen(true)] object? obj)
+	{
+		if (obj == null || GetType() != obj.GetType())
+		{
+			return false;
+		}
+
+		return this.SequenceEqual((KeyValues)obj);
+	}
+
+	public bool Equals([NotNullWhen(true)] object? obj, KeyValueComparer comparer)
+	{
+		if (obj == null || GetType() != obj.GetType())
+		{
+			return false;
+		}
+
+		return this.SequenceEqual((KeyValues)obj, comparer);
+	}
+
+	public override int GetHashCode()
+	{
+		HashCode hash = new();
+		foreach (KeyValue kv in this)
+		{
+			hash.Add(kv);
+		}
+
+		return hash.ToHashCode();
+	}
 }
