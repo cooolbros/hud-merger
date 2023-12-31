3647a4053defea700ab84ed557a4b4a6a7b88535
cooolbros <pfwobcke@gmail.com>
2023-12-31 22:58:40 +1100

    Use primary constructor in VDFSyntaxException

--- src/VDF/Exceptions/VDFSyntaxException.cs
+++ src/VDF/Exceptions/VDFSyntaxException.cs
@@ -1,12 +1,8 @@
 using System;
-using System.Diagnostics.CodeAnalysis;
 using VDF.Models;
 
 namespace VDF.Exceptions;
 
-public class VDFSyntaxException : Exception
+public class VDFSyntaxException(VDFToken? unexpected, string[] expected, int index, int line, int character) : Exception($"Unexpected {(unexpected != null ? $"{unexpected?.Type} '{unexpected?.Value}'" : "EOF")} at position {index} (line {line + 1}, character {character + 1}). Expected {string.Join(" | ", expected)}.")
 {
-	public VDFSyntaxException(VDFToken? unexpected, string[] expected, int index, int line, int character) : base($"Unexpected {(unexpected != null ? $"{unexpected?.Type} '{unexpected?.Value}'" : "EOF")} at position {index} (line {line + 1}, character {character + 1}). Expected {string.Join(" | ", expected)}.")
-	{
-	}
 }
