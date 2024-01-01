0c417054f37faf14435dfa60a5a58eb4f34eaf03
cooolbros <pfwobcke@gmail.com>
2024-01-01 16:43:42 +1100

    VDFSyntaxException surround expected strings with single quotes

--- src/VDF/Exceptions/VDFSyntaxException.cs
+++ src/VDF/Exceptions/VDFSyntaxException.cs
@@ -3,6 +3,6 @@ using VDF.Models;
 
 namespace VDF.Exceptions;
 
-public class VDFSyntaxException(VDFToken? unexpected, string[] expected, int index, int line, int character) : Exception($"Unexpected {(unexpected != null ? $"{unexpected?.Type} '{unexpected?.Value}'" : "EOF")} at position {index} (line {line + 1}, character {character + 1}). Expected {string.Join(" | ", expected)}.")
+public class VDFSyntaxException(VDFToken? unexpected, string[] expected, int index, int line, int character) : Exception($"Unexpected {(unexpected != null ? $"{unexpected?.Type} '{unexpected?.Value}'" : "EOF")} at position {index} (line {line + 1}, character {character + 1}). Expected '{string.Join("'' | ''", expected)}'.")
 {
 }
