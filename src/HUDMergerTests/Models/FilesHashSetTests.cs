98509808d3bab7418feb67e64d871f7387c8a6d1
cooolbros <pfwobcke@gmail.com>
2024-01-01 17:03:53 +1100

    Update FilesHashSetTests

--- src/HUDMergerTests/Models/FilesHashSetTests.cs
+++ src/HUDMergerTests/Models/FilesHashSetTests.cs
@@ -9,8 +9,7 @@ public class FilesHashSetTests
 	[TestMethod()]
 	public void FilesHashSetTest()
 	{
-		FilesHashSet set1 = new FilesHashSet();
-		set1.Add("resource/ui/hudplayerhealth.res");
+		FilesHashSet set1 = ["resource/ui/hudplayerhealth.res"];
 		Assert.IsTrue(set1.Contains("resource\\ui\\hudplayerhealth.res"));
 		Assert.IsFalse(set1.Add("resource/ui\\hudplayerhealth.res"));
 	}
