597f6443fc8c10dd6a3ef1c86a15fe38d8cb65a2
cooolbros <pfwobcke@gmail.com>
2024-06-04 13:14:40 +1000

    Add HUD Animations Serialize Culture test

---
+++ src/HUDAnimationsTests/HUDAnimationsSerializerTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HUDAnimations.Models;
+
+namespace HUDAnimations.Tests;
+
+[TestClass()]
+public class HUDAnimationsSerializerTests
+{
+	[TestMethod()]
+	public void SerializeTest()
+	{
+		HUDAnimationsFile animations = HUDAnimationsSerializer.Deserialize(File.ReadAllText("HUDAnimationsSerializerTests/scripts/hudanimations_tf.txt"));
+		string text = HUDAnimationsSerializer.Serialize(animations);
+
+		CultureInfo currentCulture = CultureInfo.CurrentCulture;
+
+		foreach (CultureInfo culture in CultureInfo.GetCultures(CultureTypes.AllCultures))
+		{
+			CultureInfo.CurrentCulture = culture;
+			Assert.AreEqual(text, HUDAnimationsSerializer.Serialize(animations));
+		}
+
+		CultureInfo.CurrentCulture = currentCulture;
+	}
+}
