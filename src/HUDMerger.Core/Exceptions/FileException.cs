49735e61b809ec3d84c5c756bd71d0feeaa864e8
cooolbros <pfwobcke@gmail.com>
2024-07-06 14:51:32 +1000

    Move Exceptions to HUDMerger.Core

--- src/HUDMerger.Core/Exceptions/FileException.cs
+++ src/HUDMerger.Core/Exceptions/FileException.cs
@@ -2,7 +2,7 @@ using System;
 using HUDMerger.Models;
 using VDF.Exceptions;
 
-namespace HUDMerger.Exceptions;
+namespace HUDMerger.Core.Exceptions;
 
 public class FileException(HUD hud, string relativePath, VDFSyntaxException innerException) : Exception($"{hud.Name}: {App.PathSeparatorRegex().Replace(relativePath, "/")}: {innerException.Message}")
 {
